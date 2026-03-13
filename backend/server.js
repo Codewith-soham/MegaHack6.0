3f078c879813e14d578bd0610af0698a3e6f4fe7
ghadgesoham <ghadgesoham093@gmail.com>
2026-03-13 13:26:37 +0530

    Initial project structure

---
+++ backend/server.js
@@ -0,0 +1,19 @@
+import dotenv from 'dotenv';
+import { app } from './src/app.js';
+import connectDB from './src/config/db.js';
+
+dotenv.config({             //setup dotenv to load environment variables from .env file
+    path: "./.env"
+});
+
+const port = process.env.PORT || 8000;   //get the port from environment variable or use default 8000
+
+connectDB()
+    .then(() => {
+        app.listen(port, () => {
+            console.log(`Server is running on port ${port}`);
+        })
+    })
+    .catch((err) => {
+        console.error("Failed to connect to the database", err);
+    })
